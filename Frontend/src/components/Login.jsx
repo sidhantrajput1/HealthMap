eab6ca4b488a705bff1e3fc31c02e3370386139d
sidhantrajput1 <sidhant808310@gmail.com>
2024-11-07 00:14:31 +0530

    frontend

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,64 +1,109 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleSubmit = (e) => {
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Email:", email);
-    console.log("Password:", password);
+    console.log(input);
+    try {
+      const res = await axios.post(
+        "http://localhost:4000/api/v1/user/login",
+        input,
+        {
+          headers: {
+            "content-Type": "application/json",
+          },
+          withCredentials: false,
+        }
+      );
+      if (res.data.success) {
+        setInput({
+          email: "",
+          password: "",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md bg-white shadow-md rounded-lg p-8">
-        <h2 className="text-3xl font-bold text-center mb-6">Login</h2>
-        <form onSubmit={handleSubmit}>
-          {/* Email Input */}
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md bg-white shadow-md rounded-lg p-8">
+          <h2 className="text-3xl font-bold text-center mb-6">Login Page</h2>
+          <form onSubmit={handleSubmit}>
 
-          {/* Password Input */}
-          <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
+            {/* Email Input */}
+            <div className="mb-4">
+              <div className="flex items-center space-x-3">
+                <label
+                  className="w-24 text-gray-700 text-sm font-bold"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Enter your email"
+                  value={input.email}
+                  onChange={changeEventHandler}
+                  required
+                />
+              </div>
+            </div>
 
-          {/* Login Button */}
-          <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-            >
-              Login
-            </button>
-          </div>
-        </form>
+            {/* Password Input */}
+            <div className="mb-6">
+              <div className="flex items-center space-x-3">
+                <label
+                  className="w-24 text-gray-700 text-sm font-bold"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Enter your password"
+                  value={input.password}
+                  onChange={changeEventHandler}
+                  required
+                />
+              </div>
+            </div>
+
+            {/* Sign Up Button */}
+            <div className="flex items-center justify-between">
+              <button
+                type="submit"
+                className="w-full bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+              >
+                Sign Up
+              </button>
+            </div>
+            <div className="mt-4 text-center ">
+              <Link to="/login" className="text-blue-500 hover:underline">
+                Already have an account?
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,103 +1,131 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-const Signup = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+import axios from "axios";
 
-  const handleSubmit = (e) => {
+const Signup = () => {
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Handle the signup logic here
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Password:", password);
+    console.log(input);
+    try {
+      const res = await axios.post(
+        "http://localhost:4000/api/v1/user/signup",
+        input,
+        {
+          headers: {
+            "content-Type": "application/json",
+          },
+          withCredentials: false,
+        }
+      );
+      if (res.data.success) {
+        setInput({
+          name: "",
+          email: "",
+          password: "",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md bg-white shadow-md rounded-lg p-8">
-        <h2 className="text-3xl font-bold text-center mb-6">Sign Up</h2>
-        <form onSubmit={handleSubmit}>
-          {/* Name Input */}
-          <div className="mb-4">
-            <div className="flex items-center space-x-3">
-              <label
-                className="w-24 text-gray-700 text-sm font-bold"
-                htmlFor="name"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter your name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md bg-white shadow-md rounded-lg p-8">
+          <h2 className="text-3xl font-bold text-center mb-6">Sign Up</h2>
+          <form onSubmit={handleSubmit}>
+            {/* Name Input */}
+            <div className="mb-4">
+              <div className="flex items-center space-x-3">
+                <label
+                  className="w-24 text-gray-700 text-sm font-bold"
+                  htmlFor="name"
+                >
+                  Name
+                </label>
+                <input
+                  type="name"
+                  id="name"
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Enter your name"
+                  value={input.name}
+                  onChange={changeEventHandler}
+                  required
+                />
+              </div>
             </div>
-          </div>
 
-          {/* Email Input */}
-          <div className="mb-4">
-            <div className="flex items-center space-x-3">
-              <label
-                className="w-24 text-gray-700 text-sm font-bold"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
+            {/* Email Input */}
+            <div className="mb-4">
+              <div className="flex items-center space-x-3">
+                <label
+                  className="w-24 text-gray-700 text-sm font-bold"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Enter your email"
+                  value={input.email}
+                  onChange={changeEventHandler}
+                  required
+                />
+              </div>
             </div>
-          </div>
 
-          {/* Password Input */}
-          <div className="mb-6">
-            <div className="flex items-center space-x-3">
-              <label
-                className="w-24 text-gray-700 text-sm font-bold"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+            {/* Password Input */}
+            <div className="mb-6">
+              <div className="flex items-center space-x-3">
+                <label
+                  className="w-24 text-gray-700 text-sm font-bold"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Enter your password"
+                  value={input.password}
+                  onChange={changeEventHandler}
+                  required
+                />
+              </div>
             </div>
-          </div>
 
-          {/* Sign Up Button */}
-          <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              className="w-full bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
-            >
-              Sign Up
-            </button>
-          </div>
-          <div className="mt-4 text-center ">
-            <Link to="/login" className="text-blue-500 hover:underline">
-              Already have an account?
-            </Link>
-          </div>
-        </form>
+            {/* Sign Up Button */}
+            <div className="flex items-center justify-between">
+              <button
+                type="submit"
+                className="w-full bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+              >
+                Sign Up
+              </button>
+            </div>
+            <div className="mt-4 text-center ">
+              <Link to="/login" className="text-blue-500 hover:underline">
+                Already have an account?
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
